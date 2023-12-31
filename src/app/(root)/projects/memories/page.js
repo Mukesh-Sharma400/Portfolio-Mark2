9536c50c4558af3546caf997341c1bf4799e8c68
Mukesh Sharma <83642159+Mukesh-Sharma400@users.noreply.github.com>
2023-12-31 18:53:32 +0530

    Projects Page Setup

---
+++ src/app/(root)/projects/memories/page.js
@@ -0,0 +1,27 @@
+"use client";
+
+import styled from "styled-components";
+import { useSelector } from "react-redux";
+import Footer from "@/app/components/Footer";
+import { uiState } from "../../../redux/uiSlice";
+import BaseLayout from "@/app/components/BaseLayout";
+
+export default function Memories() {
+  const { theme } = useSelector(uiState);
+
+  return (
+    <BaseLayout>
+      <Heading>Memories</Heading>
+      <Footer />
+    </BaseLayout>
+  );
+}
+
+const Heading = styled.h1`
+  color: ${({ theme }) =>
+    theme.currentTheme === "light"
+      ? theme.lightMode.whiteColor150
+      : theme.globalColors.whiteColor};
+  margin-bottom: 15px !important;
+  transition: all 0.5s ease-in-out;
+`;
