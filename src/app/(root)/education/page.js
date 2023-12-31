9830db76d09129a66f0c2dcf2c54068599a4adad
Mukesh Sharma <83642159+Mukesh-Sharma400@users.noreply.github.com>
2023-12-31 16:55:56 +0530

    Education & Experience Page

--- src/app/(root)/education/page.js
+++ src/app/(root)/education/page.js
@@ -1,10 +1,14 @@
 "use client";
 
+import Image from "next/image";
 import styled from "styled-components";
 import { useSelector } from "react-redux";
 import { uiState } from "../../redux/uiSlice";
 import BaseLayout from "@/app/components/BaseLayout";
 import Footer from "@/app/components/Footer";
+import scct from "../../../../public/assets/scct.jpeg";
+import pace from "../../../../public/assets/pace.webp";
+import tilak from "../../../../public/assets/tilak.webp";
 
 export default function Education() {
   const { theme } = useSelector(uiState);
@@ -15,6 +19,89 @@ export default function Education() {
       <Description>
         Explore academic achievements and qualifications
       </Description>
+      <EducationsWrapper>
+        <EducationWrapper>
+          <LeftSide>
+            <EducationImage
+              className="rounded-3"
+              src={scct}
+              alt="Sanpada College of Commerce & Technology"
+              width={100}
+              height={100}
+            />
+          </LeftSide>
+          <RightSide>
+            <EducationName>
+              Bachelors in Information Technology{" "}
+              <span>(September 2020 - May 2023)</span>
+            </EducationName>
+            <College>
+              Sanpada College of Commerce & Technology{" "}
+              <span>(Sanpada, Navi Mumbai)</span>
+            </College>
+            <Content>
+              I always had a passion for web development and acquired skills in
+              various web technologies. I have created many websites, which
+              impressed all my colleagues and professors. I became a
+              sought-after web developer, proving that hard work and dedication
+              can lead to success.
+            </Content>
+          </RightSide>
+        </EducationWrapper>
+        <Divider />
+        <EducationWrapper>
+          <LeftSide>
+            <EducationImage
+              className="rounded-3"
+              src={pace}
+              alt="Pace Education Society"
+              width={100}
+              height={100}
+            />
+          </LeftSide>
+          <RightSide>
+            <EducationName>
+              Higher School Certification <span>(June 2016 - May 2018)</span>{" "}
+            </EducationName>
+            <College>
+              Pace Education Society <span>(Nerul, Navi Mumbai)</span>
+            </College>
+            <Content>
+              I was always fascinated by the mysteries of the universe. Despite
+              not being the smartest student in my class, I spends every free
+              moment delving into books and videos on cosmology and
+              astrophysics.
+            </Content>
+          </RightSide>
+        </EducationWrapper>
+        <Divider />
+        <EducationWrapper>
+          <LeftSide>
+            <EducationImage
+              className="rounded-3"
+              src={tilak}
+              alt="Tilak International School"
+              width={100}
+              height={100}
+            />
+          </LeftSide>
+          <RightSide>
+            <EducationName>
+              Secondary School Certification{" "}
+              <span>(June 2015 - March 2016)</span>
+            </EducationName>
+            <College>
+              Tilak International School <span>(Ghansoli, Navi Mumbai)</span>
+            </College>
+            <Content>
+              I always had a keen interest in computers and technology, I used
+              to participate in a school computer science competition despite my
+              lack of confidence. I studied hard, boosting my confidence and
+              setting me on a path towards a successful career in technology.
+            </Content>
+          </RightSide>
+        </EducationWrapper>
+      </EducationsWrapper>
       <Footer />
     </BaseLayout>
   );
@@ -35,4 +122,84 @@ const Description = styled.p`
       ? theme.lightMode.greyColor100
       : theme.darkMode.greyColor100};
   transition: all 0.5s ease-in-out;
+`;
+
+const EducationsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 50px;
+  margin: 50px 0;
+`;
+
+const EducationWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  gap: 30px;
+`;
+
+const LeftSide = styled.div`
+  width: 20%;
+  position: relative;
+`;
+
+const EducationImage = styled(Image)`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%) !important;
+`;
+
+const RightSide = styled.div`
+  width: 80%;
+`;
+
+const EducationName = styled.p`
+  font-size: 16px;
+  margin-bottom: 10px !important;
+  color: ${({ theme }) =>
+    theme.currentTheme === "light"
+      ? theme.lightMode.whiteColor150
+      : theme.globalColors.whiteColor};
+
+  span {
+    color: ${({ theme }) =>
+      theme.currentTheme === "light"
+        ? theme.lightMode.greyColor100
+        : theme.darkMode.greyColor100};
+  }
+`;
+
+const College = styled.p`
+  font-size: 14px;
+  margin-bottom: 10px !important;
+  color: ${({ theme }) =>
+    theme.currentTheme === "light"
+      ? theme.lightMode.whiteColor150
+      : theme.globalColors.whiteColor};
+
+  span {
+    color: ${({ theme }) =>
+      theme.currentTheme === "light"
+        ? theme.lightMode.greyColor100
+        : theme.darkMode.greyColor100};
+  }
+`;
+
+const Content = styled.p`
+  font-size: 14px;
+  line-height: 20px;
+  letter-spacing: 1px;
+  color: ${({ theme }) =>
+    theme.currentTheme === "light"
+      ? theme.lightMode.greyColor100
+      : theme.darkMode.greyColor100};
+`;
+
+const Divider = styled.div`
+  width: 100%;
+  height: 2px;
+  background-color: ${({ theme }) =>
+    theme.currentTheme === "light"
+      ? theme.lightMode.whiteColor201
+      : theme.darkMode.blackColor201};
 `;
--- src/app/(root)/experience/page.js
+++ src/app/(root)/experience/page.js
@@ -1,10 +1,14 @@
 "use client";
 
+import Image from "next/image";
 import styled from "styled-components";
 import { useSelector } from "react-redux";
 import { uiState } from "../../redux/uiSlice";
 import BaseLayout from "@/app/components/BaseLayout";
 import Footer from "@/app/components/Footer";
+import appavengers from "../../../../public/assets/app-avengers.jpg";
+import nullclass from "../../../../public/assets/nullclass.png";
+import hasbasoft from "../../../../public/assets/hasbasoft.png";
 
 export default function Experience() {
   const { theme } = useSelector(uiState);
@@ -15,6 +19,88 @@ export default function Experience() {
       <Description>
         Navigate through professional milestones and achievements
       </Description>
+      <ExperiencesWrapper>
+        <ExperienceWrapper>
+          <LeftSide>
+            <CompanyImage
+              className="rounded-3"
+              src={appavengers}
+              alt="App Avengers"
+              width={100}
+              height={100}
+            />
+          </LeftSide>
+          <RightSide>
+            <CompanyName>
+              App Avengers Technologies <span>(July 2023 - Present)</span>
+            </CompanyName>
+            <Role>
+              Web Developer Intern <span>(Remote)</span>
+            </Role>
+            <Content>
+              As a Web Developer Intern at App Avengers Technologies, I'm
+              contributing to web app development with React.js, Next.js, and
+              Web3 Technologies I'm designing interfaces, implementing features,
+              and engaging in code reviews for continuous learning.
+            </Content>
+          </RightSide>
+        </ExperienceWrapper>
+        <Divider />
+        <ExperienceWrapper>
+          <LeftSide>
+            <CompanyImage
+              className="rounded-3"
+              src={nullclass}
+              alt="NullClass"
+              width={100}
+              height={100}
+            />
+          </LeftSide>
+          <RightSide>
+            <CompanyName>
+              NullClass <span>(December 2022 - January 2023)</span>{" "}
+            </CompanyName>
+            <Role>
+              Project Intern <span>(Remote)</span>
+            </Role>
+            <Content>
+              As a NullClass Project Intern, I created a responsive MERN Stack
+              app (Stack Overflow Clone) and added features like a chat bot and
+              community section. I gained valuable experience in React, Node.js,
+              Express.js, and MongoDB, improving my skills in building scalable
+              and user-centric web applications.
+            </Content>
+          </RightSide>
+        </ExperienceWrapper>
+        <Divider />
+        <ExperienceWrapper>
+          <LeftSide>
+            <CompanyImage
+              className="rounded-3"
+              src={hasbasoft}
+              alt="Hasbasoft Technology"
+              width={100}
+              height={100}
+            />
+          </LeftSide>
+          <RightSide>
+            <CompanyName>
+              Hasbasoft Technology Pvt Ltd{" "}
+              <span>(August 2021 - October 2022)</span>
+            </CompanyName>
+            <Role>
+              MERN Stack Software Trainee <span>(Remote)</span>
+            </Role>
+            <Content>
+              As a Software Trainee at Hasbasoft Technology Pvt Ltd, I built
+              several MERN Stack applications, including an Ecommerce platform.
+              Through hands-on experience, I gained expertise in React, Node.js,
+              Express.js, and MongoDB, developing dynamic and user-friendly web
+              solutions in a professional software development environment.
+            </Content>
+          </RightSide>
+        </ExperienceWrapper>
+      </ExperiencesWrapper>
       <Footer />
     </BaseLayout>
   );
@@ -35,4 +121,84 @@ const Description = styled.p`
       ? theme.lightMode.greyColor100
       : theme.darkMode.greyColor100};
   transition: all 0.5s ease-in-out;
+`;
+
+const ExperiencesWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 50px;
+  margin: 50px 0;
+`;
+
+const ExperienceWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  gap: 30px;
+`;
+
+const LeftSide = styled.div`
+  width: 20%;
+  position: relative;
+`;
+
+const CompanyImage = styled(Image)`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%) !important;
+`;
+
+const RightSide = styled.div`
+  width: 80%;
+`;
+
+const CompanyName = styled.p`
+  font-size: 16px;
+  margin-bottom: 10px !important;
+  color: ${({ theme }) =>
+    theme.currentTheme === "light"
+      ? theme.lightMode.whiteColor150
+      : theme.globalColors.whiteColor};
+
+  span {
+    color: ${({ theme }) =>
+      theme.currentTheme === "light"
+        ? theme.lightMode.greyColor100
+        : theme.darkMode.greyColor100};
+  }
+`;
+
+const Role = styled.p`
+  font-size: 14px;
+  margin-bottom: 10px !important;
+  color: ${({ theme }) =>
+    theme.currentTheme === "light"
+      ? theme.lightMode.whiteColor150
+      : theme.globalColors.whiteColor};
+
+  span {
+    color: ${({ theme }) =>
+      theme.currentTheme === "light"
+        ? theme.lightMode.greyColor100
+        : theme.darkMode.greyColor100};
+  }
+`;
+
+const Content = styled.p`
+  font-size: 14px;
+  line-height: 20px;
+  letter-spacing: 1px;
+  color: ${({ theme }) =>
+    theme.currentTheme === "light"
+      ? theme.lightMode.greyColor100
+      : theme.darkMode.greyColor100};
+`;
+
+const Divider = styled.div`
+  width: 100%;
+  height: 2px;
+  background-color: ${({ theme }) =>
+    theme.currentTheme === "light"
+      ? theme.lightMode.whiteColor201
+      : theme.darkMode.blackColor201};
 `;
