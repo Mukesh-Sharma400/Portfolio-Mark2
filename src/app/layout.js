cae36b2d8db8ba44c4bb002f1c8129f66c61243b
Mukesh Sharma <83642159+Mukesh-Sharma400@users.noreply.github.com>
2024-01-26 16:16:50 +0530

    Web Analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import "./global.css";
 import Head from "next/head";
 import { Providers } from "./providers";
+import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata = {
@@ -35,6 +36,7 @@ export default function Layout({ children }) {
       <body>
         <Providers>{children}</Providers>
         <SpeedInsights />
+        <Analytics />
         <script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
           integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
